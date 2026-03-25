6685806c133cf07f60d3224a02a96529440e1bf6
Abbassi Nabil <184096803+AbbassiNabil@users.noreply.github.com>
2026-03-25 09:06:04 +0100

    fin functionalite decouvrir et recherche

---
+++ script_decouvrir.js
@@ -0,0 +1,61 @@
+const pokemonContainer = document.getElementById("carosello");
+
+function getPokemon() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=300")
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((data) => {
+      data.results.forEach((pokemon) => {
+        pokemonCard(pokemon.url);
+      });
+    });
+}
+
+function pokemonCard(url) {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((pokemonData) => {
+      const card = document.createElement("article");
+      card.className = "pokemon-card";
+      card.setAttribute("data-name", pokemonData.name);
+
+      const img = document.createElement("img");
+      img.src = pokemonData.sprites.front_default;
+      img.alt = pokemonData.name;
+      img.style.width = "200px";
+      img.style.backgroundColor = "whitesmoke";
+      img.style.border = "1px solid black";
+      img.style.borderRadius = "10px";
+
+      const name = document.createElement("h3");
+      name.textContent = pokemonData.name.toUpperCase();
+
+      const num = document.createElement("p");
+      num.textContent = "#" + pokemonData.id;
+
+      const btnDetails = document.createElement("button");
+      btnDetails.textContent = "Voir details";
+      btnDetails.style.backgroundColor = "red";
+      btnDetails.style.color = "white";
+      btnDetails.addEventListener("click", () => showDetails(pokemonData));
+
+      const btnTeam = document.createElement("button");
+      btnTeam.textContent = "Ajouter a l'equipe";
+      btnTeam.style.backgroundColor = "white";
+      btnTeam.addEventListener("click", () => addToTeam(pokemonData.name));
+
+      card.appendChild(img);
+      card.appendChild(name);
+      card.appendChild(num);
+      card.appendChild(btnDetails);
+      card.appendChild(btnTeam);
+
+      pokemonContainer.appendChild(card);
+    });
+}
+
+getPokemon();
---
+++ script_index.js
@@ -0,0 +1,66 @@
+const serchInput = document.getElementById("search-input");
+const serchBtn = document.getElementById("search-btn");
+const container = document.getElementById("pokemon-container");
+
+function createPokemonCard(data) {
+  container.textContent = "";
+
+  const card = document.createElement("div");
+  card.classList.add("pokemon-card");
+  card.style.width = "300px";
+
+  const nom = document.createElement("h2");
+  nom.textContent = data.name.toUpperCase() + " #" + data.id;
+
+  const img = document.createElement("img");
+  img.src = data.sprites.front_default;
+  img.alt = data.name;
+  img.style.width = "300px";
+
+  const type = document.createElement("p");
+
+  type.textContent = "Type: " + data.types[0].type.name;
+
+  const btnDetails = document.createElement("button");
+  btnDetails.textContent = "Voir details";
+  btnDetails.style.padding = "10px";
+  btnDetails.style.backgroundColor = "red";
+  btnDetails.style.color = "white";
+
+  btnDetails.addEventListener("click", () => showDetails(pokemonData));
+
+  const btnTeam = document.createElement("button");
+  btnTeam.textContent = "Ajouter à l'equipe";
+  btnTeam.style.padding = "10px";
+  btnTeam.style.backgroundColor = "white";
+  btnTeam.addEventListener("click", () => addToTeam(pokemonData.name));
+
+  card.appendChild(btnDetails);
+  card.appendChild(btnTeam);
+  card.appendChild(img);
+  card.appendChild(nom);
+  card.appendChild(type);
+  container.appendChild(card);
+}
+
+function searchPokemon() {
+  const value = serchInput.value.toLowerCase();
+
+  if (value === "") return;
+
+  fetch("https://pokeapi.co/api/v2/pokemon/" + value)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      createPokemonCard(data);
+    });
+}
+
+serchBtn.addEventListener("click", searchPokemon);
+
+serchInput.addEventListener("keypress", function (a) {
+  if (a.key === "Enter") {
+    searchPokemon();
+  }
+});
